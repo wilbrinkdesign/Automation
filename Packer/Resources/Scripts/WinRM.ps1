486f509b98708b4fb23a485a18d27b700298a6aa
Mark Wilbrink <mark@wilbrink.design>
2024-01-05 11:46:34 +0100

    Drop WinRM HTTPS

--- Packer/Resources/Scripts/WinRM.ps1
+++ Packer/Resources/Scripts/WinRM.ps1
@@ -1,7 +1,5 @@
-New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "WinRMCertificate"
 Enable-PSRemoting -SkipNetworkProfileCheck -Force
-($cert = gci Cert:\LocalMachine\My\) -and (New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $cert.Thumbprint -Force)
-New-NetFirewallRule -DisplayName 'Windows Remote Management (HTTPS-In)' -Name 'Windows Remote Management (HTTPS-In)' -Profile Any -LocalPort 5986 -Protocol TCP
+New-NetFirewallRule -DisplayName 'Windows Remote Management (HTTP-In)' -Name 'Windows Remote Management (HTTP-In)' -Profile Any -LocalPort 5985 -Protocol TCP
 Set-Item WSMan:\localhost\Service\Auth\Basic -Value $true
 Set-Item WSMan:\localhost\Service\AllowUnencrypted $true -Force
 Restart-Service WinRM
